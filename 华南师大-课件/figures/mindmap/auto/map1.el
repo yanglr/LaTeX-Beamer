2207e6551833ecedc6811378c5bc988b8fc364aa
spracle <spracle@gmail.com>
2014-03-09 22:00:57 +0800

    io

---
+++ 华南师大-课件/figures/mindmap/auto/map1.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "map1"
+ (lambda ()
+    (TeX-run-style-hooks
+     "tikz"
+     "xeCJK"
+     "latex2e"
+     "art10"
+     "article")))
+
---
+++ 华南师大-课件/figures/mindmap/auto/map6.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "map6"
+ (lambda ()
+    (TeX-run-style-hooks
+     "tikz"
+     "xeCJK"
+     "latex2e"
+     "art10"
+     "article")))
+
